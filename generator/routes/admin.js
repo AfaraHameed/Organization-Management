f16542411daedcafaeaa8f84b116b1870d46749f
Afara <afarasidheek@gmail.com>
2022-01-04 10:13:22 +0400

    add installemnt and add loan page with dummy content

--- generator/routes/admin.js
+++ generator/routes/admin.js
@@ -25,4 +25,41 @@ router.get('/', function(req, res, next) {
   res.render('admin/view_members',{admin : true , members});
 });
 
+//member details page
+router.get('/members_details', function (req,res,next){
+
+    let memberDetail=[{
+      username : "aaa",
+      email : "ccc@gmail.com",
+      monthly_amount : 1122,
+      date_m : '22/22/22',
+      loan_amount:111,
+      date_l:'22/22/22'
+
+    },
+    {
+      username : "aaa",
+      email : "ccc@gmail.com",
+      monthly_amount : 1122,
+      date_m : '22/22/22',
+     
+
+    },
+    {
+      username : "aaa",
+      email : "ccc@gmail.com",
+      monthly_amount : 1122,
+      date_m : '22/22/22',
+      loan_amount:111,
+      date_l:'22/22/22'
+
+    }]
+    res.render('admin/members_details',{admin:true,memberDetail})
+})
+
+router.get('/add_installment',(req,res,next)=>{
+
+  res.render('admin/add_installment',{admin:true})
+})
+
 module.exports = router;
